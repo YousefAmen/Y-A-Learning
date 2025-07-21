c5bfc2bb22cc69783f2121e8b2ff369e3651869b
Yousef <ammenyoussef@gmail.com>
2025-07-21 19:09:51 +0300

    fix some bugs and add cart system to allow users to addcourses in his owen carts

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from .views import add_to_cart, cart_detail, remove_from_cart
+
+app_name = 'cart'
+
 urlpatterns = [
-  
+    path('', cart_detail, name='cart_detail'),
+    path('add/', add_to_cart, name='add-to-cart'),
+    path('remove/', remove_from_cart, name='remove-from-cart'), 
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from course.models import Course
+
+from .cart import Cart
+
+
+def add_to_cart(request):
+  if request.method == "POST" and request.POST.get("action") == "post":
+    token = request.POST.get('token')
+    course = get_object_or_404(Course, token=token)
+    cart = Cart(request)
+    cart.add_item(course)
+    return JsonResponse({
+    'success': True,
+    'total_amount': cart.total_amount(),
+    'item_count': len(cart),
+  })
+  
+
+def remove_from_cart(request):
+  if request.method == "POST" and request.POST.get("action") == "post":
+    token = request.POST.get('token')
+    course = get_object_or_404(Course, token=token)
+    cart = Cart(request)
+    cart.remove_item(course)
+    return JsonResponse({
+    'success': True,
+    'total_amount': cart.total_amount(),
+    'item_count': len(cart),
+  })
+  
+
+def cart_detail(request):
+  cart  = Cart(request)
+  courses = cart.courses()
+  return render(request,'cart/cart_detail.html',{"cart":cart,"courses":courses})
+  
