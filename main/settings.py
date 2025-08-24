1a67140248b2c48facc5e7f8a37ea5248e601ae9
yousef Amen <ammenyoussef@gmail.com>
2025-08-24 18:33:16 +0300

    fix DB_PASSWORD issue

--- main/settings.py
+++ main/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
-        "PASSWORD": os.getenv["DB_PASSWORD"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
         "HOST": "switchback.proxy.rlwy.net",
         "PORT": "14628",
     }
