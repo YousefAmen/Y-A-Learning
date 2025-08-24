4022870946f0d7018a264ea1a6b4123e022def3b
yousef Amen <ammenyoussef@gmail.com>
2025-08-24 20:36:22 +0300

    fix the procfile

--- main/settings.py
+++ main/settings.py
@@ -73,18 +73,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # django allauth middleware
     "allauth.account.middleware.AccountMiddleware",
-    # whitenoise package
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
-
 ROOT_URLCONF = "main.urls"
 
 TEMPLATES = [
