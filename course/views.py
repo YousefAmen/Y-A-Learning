259cd19d4d7d1d8cb3fd8eda6358159b780b2cd6
yousef Amen <ammenyoussef@gmail.com>
2025-08-30 17:55:02 +0300

    put some data in index.html for test

--- course/views.py
+++ course/views.py
@@ -57,8 +57,10 @@ class Index(TemplateView):
             .annotate(students=Count("instructor_courses__course_enrollments"))
             .order_by("-students")
         )
-        from main.settings import twitter_id, twitter_secret
+        from main.settings import twitter_id, twitter_secret, google_secret, google_id
 
+        context["google_secret"] = google_secret
+        context["google_id"] = google_id
         context["twitter_id"] = twitter_id
         context["twitter_secret"] = twitter_secret
         context["categories"] = top_categories
