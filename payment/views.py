56222efb19828d68a3e1fe7bbdc16bc4c5126ea9
yousef Amen <ammenyoussef@gmail.com>
2025-08-31 18:48:52 +0300

    fix the small issus

--- payment/views.py
+++ payment/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.decorators.http import require_POST
-from django.conf import settings
+from main import settings
 from django.urls import reverse
 from django.views import View
 from django.db import transaction
