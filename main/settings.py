997e4278c49034c6bd80251de5e850076ba8a6be
yousef Amen <ammenyoussef@gmail.com>
2025-09-05 20:43:04 +0300

    adding subtitle

--- main/settings.py
+++ main/settings.py
@@ -83,9 +83,9 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
 ]
 ROOT_URLCONF = "main.urls"
 
