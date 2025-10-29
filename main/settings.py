5a752d56d3358594b2dc7e4a68a960ea2af7b2ca
yousef Amen <ammenyoussef@gmail.com>
2025-10-29 15:11:52 +0300

    delete debugtollbar

--- main/settings.py
+++ main/settings.py
@@ -131,7 +131,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
-        "PASSWORD": os.environ["DB_PASSWORD"],
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": "centerbeam.proxy.rlwy.net",
         "PORT": "28103",
     }
