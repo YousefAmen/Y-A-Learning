e3842564c4f16692a43957de0f16630452edc414
yousef Amen <ammenyoussef@gmail.com>
2025-10-29 14:13:00 +0300

    fix the db password

--- main/settings.py
+++ main/settings.py
@@ -132,7 +132,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
-        "PASSWORD": os.environ["DB_Password"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
         "HOST": "centerbeam.proxy.rlwy.net",
         "PORT": "28103",
     }
